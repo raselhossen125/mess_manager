dc6685a9424f9594c84062bc25125be383acdf8f
rasel-hossen <straselhossen24@gmail.com>
2022-07-30 20:01:15 +0600

    profile edit

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -26,124 +26,134 @@ class ProfilePage extends StatelessWidget {
           },
         ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-        child: Column(
-          children: [
-            Card(
-              elevation: 4,
-              child: FutureBuilder(
-                future: getData(context),
-                builder: (context, snapshort) {
-                  if (snapshort.hasData) {
-                    return model?.managerImageUrl == null
-                        ? Image.asset(
-                            'images/R.png',
-                            height: 250,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          )
-                        : Image.file(
-                            File(model!.managerImageUrl.toString()),
-                            height: 250,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+          child: Column(
+            children: [
+              Card(
+                elevation: 4,
+                child: FutureBuilder(
+                  future: getData(context),
+                  builder: (context, snapshort) {
+                    if (snapshort.hasData) {
+                      return model?.managerImageUrl == null
+                          ? Image.asset(
+                              'images/R.png',
+                              height: 250,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.file(
+                              File(model!.managerImageUrl.toString()),
+                              height: 250,
+                              width: double.infinity,
+                              fit: BoxFit.cover,
+                            );
+                    }
+                    return CircularProgressIndicator(
+                      color: Colors.white,
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 5),
+              Card(
+                elevation: 4,
+                child: Column(
+                  children: [
+                    FutureBuilder(
+                      future: getData(context),
+                      builder: (context, snapShort) {
+                        if (snapShort.hasData) {
+                          return ListTile(
+                            title: Text(model!.managerName),
+                            trailing: IconButton(
+                              onPressed: () {
+                                showConfirmationDialog(context, 'Edit Manager Name', model!.managerName);
+                              },
+                              icon: Icon(
+                                Icons.edit,
+                                size: 20,
+                                color: CustomColors.appColor,
+                              ),
+                            ),
                           );
-                  }
-                  return CircularProgressIndicator(
-                    color: Colors.white,
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 3),
-            Card(
-              elevation: 4,
-              child: Column(
-                children: [
-                  FutureBuilder(
-                    future: getData(context),
-                    builder: (context, snapShort) {
-                      if (snapShort.hasData) {
-                        return ListTile(
-                          title: Text(model!.managerName),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit,
-                              size: 20,
-                              color: CustomColors.appColor,
+                        }
+                        return CircularProgressIndicator();
+                      },
+                    ),
+                    Divider(),
+                    FutureBuilder(
+                      future: getData(context),
+                      builder: (context, snapShort) {
+                        if (snapShort.hasData) {
+                          return ListTile(
+                            title: Text(model!.managerEmail),
+                            trailing: IconButton(
+                              onPressed: () {
+                                showConfirmationDialog(context, 'Edit Manager Email', model!.managerEmail);
+                              },
+                              icon: Icon(
+                                Icons.edit,
+                                size: 20,
+                                color: CustomColors.appColor,
+                              ),
                             ),
-                          ),
-                        );
-                      }
-                      return CircularProgressIndicator();
-                    },
-                  ),
-                  Divider(),
-                  FutureBuilder(
-                    future: getData(context),
-                    builder: (context, snapShort) {
-                      if (snapShort.hasData) {
-                        return ListTile(
-                          title: Text(model!.managerEmail),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit,
-                              size: 20,
-                              color: CustomColors.appColor,
+                          );
+                        }
+                        return CircularProgressIndicator();
+                      },
+                    ),
+                    Divider(),
+                    FutureBuilder(
+                      future: getData(context),
+                      builder: (context, snapShort) {
+                        if (snapShort.hasData) {
+                          return ListTile(
+                            title: Text(model!.managerphone),
+                            trailing: IconButton(
+                              onPressed: () {
+                                showConfirmationDialog(context, 'Edit Manager Phone', model!.managerphone);
+                              },
+                              icon: Icon(
+                                Icons.edit,
+                                size: 20,
+                                color: CustomColors.appColor,
+                              ),
                             ),
-                          ),
-                        );
-                      }
-                      return CircularProgressIndicator();
-                    },
-                  ),
-                  Divider(),
-                  FutureBuilder(
-                    future: getData(context),
-                    builder: (context, snapShort) {
-                      if (snapShort.hasData) {
-                        return ListTile(
-                          title: Text(model!.managerphone),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit,
-                              size: 20,
-                              color: CustomColors.appColor,
+                          );
+                        }
+                        return CircularProgressIndicator();
+                      },
+                    ),
+                    Divider(),
+                    FutureBuilder(
+                      future: getData(context),
+                      builder: (context, snapShort) {
+                        if (snapShort.hasData) {
+                          return ListTile(
+                            title: Text(model!.manageruniversity),
+                            trailing: IconButton(
+                              onPressed: () {
+                                showConfirmationDialog(context, 'Edit Manager University', model!.manageruniversity);
+                              },
+                              icon: Icon(
+                                Icons.edit,
+                                size: 20,
+                                color: CustomColors.appColor,
+                              ),
                             ),
-                          ),
-                        );
-                      }
-                      return CircularProgressIndicator();
-                    },
-                  ),
-                  Divider(),
-                  FutureBuilder(
-                    future: getData(context),
-                    builder: (context, snapShort) {
-                      if (snapShort.hasData) {
-                        return ListTile(
-                          title: Text(model!.manageruniversity),
-                          trailing: IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.edit,
-                              size: 20,
-                              color: CustomColors.appColor,
-                            ),
-                          ),
-                        );
-                      }
-                      return CircularProgressIndicator();
-                    },
-                  ),
-                ],
-              ),
-            )
-          ],
+                          );
+                        }
+                        return CircularProgressIndicator();
+                      },
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
@@ -153,5 +163,44 @@ class ProfilePage extends StatelessWidget {
     model = await Provider.of<DBProvider>(context, listen: false)
         .getLogInPersonByManagerId(context) as RegisterModel;
     return model;
+  }
+
+  void showConfirmationDialog(BuildContext context, String title, value) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: TextFormField(
+                // controller: email_Controller,
+                cursorColor: CustomColors.appColor,
+                keyboardType: TextInputType.emailAddress,
+                initialValue: value,
+                style: TextStyle(
+                    color: CustomColors.appColor, fontWeight: FontWeight.w500),
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white.withOpacity(0.9),
+                  contentPadding: EdgeInsets.only(left: 10),
+                  focusColor: Colors.white,
+                  prefixIcon: Icon(
+                    Icons.email,
+                    color: CustomColors.appColor,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide.none,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+        actions: [
+          TextButton(onPressed: () {
+            Navigator.pop(context, false);
+          }, child: Text('No')),
+          TextButton(onPressed: () {
+            Navigator.pop(context, true);
+          }, child: Text('Submit'))
+        ],
+      ),
+    );
   }
 }
